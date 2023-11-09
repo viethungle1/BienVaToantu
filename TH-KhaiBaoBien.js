1a66ed002a91f4cb39eb2bf70a1e0c89ce89dfd1
viethungle1 <hunglv0801@gmail.com>
2023-11-09 16:47:07 +0700

    variableNlogical

---
+++ TH-KhaiBaoBien.js
@@ -0,0 +1,11 @@
+let a = parseInt(prompt("nhap a:"));
+let b = parseInt(prompt("nhap b:"));
+let result = a%b;
+if(result == 0){
+    alert("a là bội số của b");}
+else{
+    alert("a không phải là bội số của b");
+    8.}
+
+
+
